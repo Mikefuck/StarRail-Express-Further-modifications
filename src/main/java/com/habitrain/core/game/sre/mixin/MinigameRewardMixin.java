c56c554de73a7dfe0431967af7951e6de82b531d
Mike <mike@habitrain.dev>
2026-07-09 21:19:14 +0800

    batch4: mixin required downgrade for fragile targets

--- src/main/java/com/habitrain/core/game/sre/mixin/MinigameRewardMixin.java
+++ src/main/java/com/habitrain/core/game/sre/mixin/MinigameRewardMixin.java
@@ -8,10 +8,12 @@ import net.minecraft.server.level.ServerPlayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Pseudo
 @Mixin(targets = "io.wifi.starrailexpress.cca.SREPlayerMinigameTaskComponent", remap = false)
 public class MinigameRewardMixin {
 
